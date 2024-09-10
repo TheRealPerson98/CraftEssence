5d6d6dcc926d40020d4eca3db5decf42ccd24b7c
Person98 <jacesleeman603@gmail.com>
2024-09-10 15:47:57 -0400

    Add custom events for user join and leave
    Introduced `UserJoinEvent` and `UserLeaveEvent` to handle user logins and logouts more effectively. Updated `UserListener` to replace logging with custom event calls on player join and quit.

--- essences/commonsessence/src/main/java/com/person98/commonsessence/user/event/UserListener.java
+++ essences/commonsessence/src/main/java/com/person98/commonsessence/user/event/UserListener.java
@@ -3,6 +3,8 @@ package com.person98.commonsessence.user.event;
 import com.person98.commonsessence.CommonsEssence;
 import com.person98.commonsessence.event.Events;
 import com.person98.commonsessence.user.UserManager;
+import com.person98.commonsessence.user.event.events.UserJoinEvent;
+import com.person98.commonsessence.user.event.events.UserLeaveEvent;
 import com.person98.craftessence.util.logging.EssenceLogger;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -20,13 +22,13 @@ public class UserListener {
     private void userEvents() {
         Events.hook(PlayerJoinEvent.class, event -> {
             UserManager.markUserOnline(event.getPlayer().getUniqueId());
-            EssenceLogger.Info("Player " + event.getPlayer().getName() + " is marked as online.");
+            Events.call(new UserJoinEvent(UserManager.getUser(event.getPlayer().getUniqueId())));
         }).bindTo(essence);
 
         // Hook into PlayerQuitEvent to mark the player as offline
         Events.hook(PlayerQuitEvent.class, event -> {
             UserManager.markUserOffline(event.getPlayer().getUniqueId());
-            EssenceLogger.Info("Player " + event.getPlayer().getName() + " is marked as offline.");
+            Events.call(new UserLeaveEvent(UserManager.getUser(event.getPlayer().getUniqueId())));
         }).bindTo(essence);
     }
 }
---
+++ essences/commonsessence/src/main/java/com/person98/commonsessence/user/event/events/UserJoinEvent.java
@@ -0,0 +1,33 @@
+package com.person98.commonsessence.user.event.events;
+
+import com.person98.commonsessence.user.User;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.entity.Player;
+
+public class UserJoinEvent extends Event {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+    private final User user;
+
+    public UserJoinEvent(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Player getPlayer() {
+        return user.getPlayer();
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+}
